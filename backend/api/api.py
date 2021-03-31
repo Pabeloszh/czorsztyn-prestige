bb9943f3d58315cd7cd2dd30c0a3e32f431ae1c8
ubuntu <matifikus1@gmail.com>
2021-03-31 20:17:45 +0200

    Merged backend settings with frontend

--- backend/api/api.py
+++ backend/api/api.py
@@ -19,20 +19,3 @@ class Aps(viewsets.ModelViewSet):
     serializer_class = PicSerializer
     http_method_names = ['get']
 
-
-
-class ApOne(viewsets.ModelViewSet):
-    queryset = ApartmentsPics.objects.filter(ap_number=1)
-    permission_classes = [
-        permissions.AllowAny
-    ]
-    serializer_class = PicSerializer
-    http_method_names = ['get']
-
-class ApTwo(viewsets.ModelViewSet):
-    queryset = ApartmentsPics.objects.filter(ap_number=2)
-    permission_classes = [
-        permissions.AllowAny
-    ]
-    serializer_class = PicSerializer
-    http_method_names = ['get']
--- backend/api/models.py
+++ backend/api/models.py
@@ -1,9 +1,5 @@
 from django.db import models
 
-# Create your models here.
-
-
-
 APPNUMBER_CHOICES= [
     (1, '1'),
     (2, '2'),
@@ -11,20 +7,9 @@ APPNUMBER_CHOICES= [
     (0, 'other'),
     ]
 
-
-
-
 class ApartmentsPics(models.Model):
     ap_number = models.SmallIntegerField(null=False, blank=False, choices=APPNUMBER_CHOICES)
     title = models.CharField(max_length=100)
     placement = models.CharField(max_length=100, null=True, blank=True, default=None)
     pic = models.ImageField(upload_to='photos', null=False, blank=False)
     gallery = models.BooleanField()
-
-
-
-
-
-
-#SITE_HEADING_PIC - Pierwsze glowne zdjecie
-#AP
